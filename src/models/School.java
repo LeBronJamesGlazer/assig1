dfc9c0f0eba83e1993afb1ca9b421c86d5f036aa
LeBronJamesGlazer <spermabobra170@gmail.com>
2025-01-06 14:11:47 +0500

    School.java

--- src/models/School.java
+++ src/models/School.java
@@ -1,3 +1,5 @@
+package models;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,23 +7,23 @@ public class School {
     private List<Person> members;
 
     public School() {
-        this.members = new ArrayList<>();
+        members = new ArrayList<>();
     }
 
     public void addMember(Person person) {
         members.add(person);
     }
 
-    @Override
-    public String toString() {
-        StringBuilder schoolInfo = new StringBuilder();
-        for (Person member : members) {
-            schoolInfo.append(member.toString()).append("\n");
-        }
-        return schoolInfo.toString();
+    public List<Person> getMembers() {
+        return members;
     }
 
-    public void sortMembersBySurname() {
-        members.sort((p1, p2) -> p1.toString().split(" ")[2].compareTo(p2.toString().split(" ")[2]));
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Person member : members) {
+            sb.append(member.toString()).append("\n");
+        }
+        return sb.toString();
     }
 }
