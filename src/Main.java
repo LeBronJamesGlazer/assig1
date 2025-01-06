6bbb5cd9cf4e4ce7bef7f48e36c2e3005936bd27
LeBronJamesGlazer <spermabobra170@gmail.com>
2025-01-06 13:33:57 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import models.Person;
+import models.School;
+import models.Student;
+import models.Teacher;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        School school = new School();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            BufferedReader studentReader = new BufferedReader(new FileReader("src/students.txt"));
+            String line;
+            while ((line = studentReader.readLine()) != null) {
+                String[] data = line.split(" ");
+                String name = data[0];
+                String surname = data[1];
+                int age = Integer.parseInt(data[2]);
+                boolean gender = data[3].equalsIgnoreCase("Male");
+
+                Student student = new Student(name, surname, age, gender);
+                for (int i = 4; i < data.length; i++) {
+                    student.addGrade(Integer.parseInt(data[i]));
+                }
+                school.addMember(student);
+            }
+            studentReader.close();
+
+            BufferedReader teacherReader = new BufferedReader(new FileReader("src/teachers.txt"));
+            while ((line = teacherReader.readLine()) != null) {
+                String[] data = line.split(" ");
+                String name = data[0];
+                String surname = data[1];
+                int age = Integer.parseInt(data[2]);
+                boolean gender = data[3].equalsIgnoreCase("Male");
+                String subject = data[4];
+                int yearsOfExperience = Integer.parseInt(data[5]);
+                int salary = Integer.parseInt(data[6]);
+
+                Teacher teacher = new Teacher(name, surname, age, gender, subject, yearsOfExperience, salary);
+                if (yearsOfExperience > 10) {
+                    teacher.giveRaise(10);
+                }
+                school.addMember(teacher);
+            }
+            teacherReader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error reading files: " + e.getMessage());
+        }
+
+        System.out.println("School Members:");
+        System.out.println(school);
+
+        System.out.println("GPA Calculations:");
+        for (Person member : school.getMembers()) {
+            if (member instanceof Student) {
+                Student student = (Student) member;
+                System.out.println(student.getName() + "'s GPA: " + student.calculateGPA());
+            }
+        }
+
+        System.out.println("\nUpdated Salaries for Teachers:");
+        for (Person member : school.getMembers()) {
+            if (member instanceof Teacher) {
+                Teacher teacher = (Teacher) member;
+                System.out.println(teacher.getName() + "'s salary: " + teacher.getSalary());
+            }
         }
     }
 }
